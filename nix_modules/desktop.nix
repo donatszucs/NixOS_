14c0f89dedcd241a18e942724a732228a2001d54
donatszucs <szucs.donat7@gmail.com>
2026-06-01 16:33:14 +0200

    trying some stuff

--- nix_modules/desktop.nix
+++ nix_modules/desktop.nix
@@ -8,7 +8,11 @@
   };
 
   # Enable Hyprland
-  programs.hyprland.enable = true;
+  {
+    programs.hyprland.enable = true;
+    # Optional, hint electron apps to use wayland:
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  }
 
   # Enable dconf for application settings
   programs.dconf.enable = true;
