8fbcb186f831502ae200823a794aa61ec066063f
donatszucs <szucs.donat7@gmail.com>
2026-09-13 14:45:35 +0200

    chore: remove Keychron mouse battery script from startup commands

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -50,7 +50,6 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user start sunshine")
 
     hl.exec_cmd("~/nixos-config/scripts/WallpaperSetup/init-hyprpaper.sh &")
-    hl.exec_cmd("~/nixos-config/scripts/KeychronMouse/mouse-battery &")
     
     -- GTK, KDE and Cursor Theming
     hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-mocha-mauve-standard+normal'")
